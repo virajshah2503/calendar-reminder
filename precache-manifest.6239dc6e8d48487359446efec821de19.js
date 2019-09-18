07a803fd71e8ef13fa9a7bca8329bb8d3e8b0581
unknown <viraj>
2019-09-18 19:13:23 +0530

    Updates

---
+++ precache-manifest.6239dc6e8d48487359446efec821de19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b66b2cefc19adf8ee1d",
+    "url": "/calendar-reminder/static/js/runtime~main.0b66b2ce.js"
+  },
+  {
+    "revision": "f0e92c036e57a61fab0f",
+    "url": "/calendar-reminder/static/js/main.f0e92c03.chunk.js"
+  },
+  {
+    "revision": "768840086b95d275f11b",
+    "url": "/calendar-reminder/static/js/1.76884008.chunk.js"
+  },
+  {
+    "revision": "f0e92c036e57a61fab0f",
+    "url": "/calendar-reminder/static/css/main.e178472c.chunk.css"
+  },
+  {
+    "revision": "395b8bb623c3764887bf4d7c59586cae",
+    "url": "/calendar-reminder/index.html"
+  }
+];
